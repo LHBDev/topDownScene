04cd778bbe3a63529b18a7e81f30d352fb5e0b94
Ruben Baeza <rbaeza@tableau.com>
2016-07-20 15:57:58 -0500

    initial upload

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform player;
+    public float smooth = 10f;
+    public float cameraHeight = 30;
+    public float behindPlayer = 20;
+
+    // Update is called once per frame
+	void Update () {
+        Vector3 newPos = new Vector3(player.position.x, player.position.y + cameraHeight, player.position.z - behindPlayer);
+        transform.position = newPos;
+	}
+}
---
+++ Assets/TopDownCotnroller.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopDownCotnroller : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    public float speed = 4;
+
+    Vector3 lookPos;
+
+    GameObject cam;
+
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        cam = GameObject.FindWithTag("MainCamera");
+	}
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 100))
+        {
+            lookPos = hit.point;
+        }
+
+        Vector3 lookDir = lookPos - transform.position;
+        lookDir.y = 0;
+
+        transform.LookAt(transform.position + lookDir, Vector3.up);
+
+        //cam.transform.position = new Vector3(transform.position.x, 10, transform.position.z);
+    }
+	
+	void FixedUpdate () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        rigidBody.AddForce(movement * speed / Time.deltaTime);
+	}
+}
